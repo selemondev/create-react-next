1f2353e34666d9b36a2564123d1943d887131828
selemondev <selemondev19@gmail.com>
2024-02-18 17:51:23 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Create React Next CLI ✨
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const greeting = 'The Next Generation React Scaffolding Tool ✨'
+
+function greet() {
+  return greeting
+}
+greet()
