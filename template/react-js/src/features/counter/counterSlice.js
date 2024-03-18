2618f2913d189a1c020597474143339daf32932a
Selemon Dev <selemon@nathandigital.com>
2024-03-18 16:50:40 +0300

    feat: react-js template

---
+++ template/react-js/src/features/counter/counterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// Define the initial state using that type
+const initialState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+  },
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export const selectCount = (state) => state.counter.value
+
+export default counterSlice.reducer
